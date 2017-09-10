883e1a73fcdfd481cea2b5b0924263a729c10414
Juan Carlos Hinojosa <jch@linkear.net>
2017-09-09 23:56:18 -0500

    Otros ejemplos del modulo 2 resultos

---
+++ jke/prompt.js
@@ -0,0 +1,15 @@
+let NumOne = parseInt(prompt());
+let NumTwo = parseInt(prompt());
+
+let add = NumOne + NumTwo;
+alert("add: " + NumOne + " + " + NumTwo + " = " + add);
+
+
+let less = NumOne - NumTwo;
+alert("less: " + NumOne + " - " + NumTwo + " = " + less);
+
+let divide = NumOne / NumTwo;
+alert("divide: " + NumOne + " / " + NumTwo + " = " + divide);
+
+let multiply = NumOne * NumTwo;
+alert("multiply: " + NumOne + " * " + NumTwo + " = " + multiply);
